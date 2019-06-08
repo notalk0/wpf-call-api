6ba42bdde1de54cebd564214d26b928345dfc415
Reina Liew <notalk_0@hotmail.com>
2019-06-08 14:04:50 +0800

    Initial Commit

---
+++ DemoLibrary/ApiHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; } // static: open once per application
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("http://xkcd.com/");
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
